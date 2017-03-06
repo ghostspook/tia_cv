fa604f69b50010bf7e49554e8cceee916416aff4
ghostspook <pcastill@mac.com>
2017-03-06 00:18:53 -0500

    Nuevas instrucciones - hoja de vida

---
+++ Controllers/HojaDeVidaController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TIA_CV.Controllers
+{
+    public class HojaDeVidaController : Controller
+    {
+        public IActionResult Instrucciones()
+        {
+            return View();
+        }
+
+    }
+}
